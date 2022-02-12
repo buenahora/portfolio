8a84ab6935fbc67666eb163b5e1942c23fd41c8b
buenahora <buenahorafranco@gmail.com>
2022-02-11 23:11:45 -0300

    casi responsive

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ let nav = document.querySelector("nav")
 let ul = document.querySelector("ul")
 
 
-const verificar = () => {
+if (window.matchMedia("(max-width: 600px)").matches) {
+
+    // as.forEach( (element) => {
+    //     element.style.display = 'none'
+    // })
+    icono.classList.add("fa-solid", "fa-bars", "barras", "fa-xl")
+    icono.id = 'menu'
+    nav.insertBefore(icono, nav.firstChild)
+
+
+} else {
+
+    nav.removeChild(icono)
+}
+
+
+window.addEventListener('resize', function(event){
     if (window.matchMedia("(max-width: 600px)").matches) {
 
         // as.forEach( (element) => {
@@ -18,28 +34,25 @@ const verificar = () => {
 
         nav.removeChild(icono)
     }
-}
-
-verificar();
-
-window.addEventListener('resize', function(event){
-    verificar();
 });
 
 
 
 icono.addEventListener("click", () => {
 
-    if (ul.style.display === "block") {
+    if (ul.style.display === "flex") {
 
         ul.style.display = "none"
         icono.style.display = "block"
         icono.classList.remove('fa-solid', 'fa-xmark', 'fa-xl')
         icono.classList.add("fa-solid", "fa-bars", "barras", "fa-xl")
 
+
     } else {
 
-        ul.style.display = "block"
+        ul.style.display = "flex"
+        ul.style.flexDirection = "column"
+        ul.style.alignItems = "flex-start"
         icono.classList.remove("fa-solid", "fa-bars", "fa-xl")
         icono.classList.add('fa-solid', 'fa-xmark', 'fa-xl')
 
